1914f86aa72e4d388d43d936a9582f81a7e03afe
Yorix <yorix82@gmail.com>
2017-11-27 12:57:30 +0200

    1.0

---
+++ src/LoadFile.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class LoadFile {
+
+    File file() {
+        JFileChooser chooser = new JFileChooser(".");
+        chooser.setFileFilter(new FileNameExtensionFilter("Только текстовый файл", "txt"));
+        int returnVal = chooser.showOpenDialog(new JFrame());
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile();
+        }
+        System.exit(0);
+        return null;
+    }
+
+    String[] read(File file) throws FileNotFoundException {
+        Scanner fileReader = new Scanner(file);         // Создание сканнера файла
+        ArrayList<String> lines = new ArrayList<>();    // Создать объект списка для хранения строк из файла
+        int x = 0;
+        while (fileReader.hasNextLine()) {              // Пока файл не кончился
+            lines.add(fileReader.nextLine().trim());    // Назначить следующему элементу списка следующую строку, обрезав пробелы
+
+            // Удаляем пустые строки
+            if (lines.get(x).trim().equals(""))
+                lines.remove(x--);
+            x++;
+        }
+        return lines.toArray(new String[lines.size()]);
+    }
+}
